f475ed3a20dbc0338e471acbe4e79f292f45a5e8
Eleanor Lee <ellie007@gmail.com>
2014-11-04 17:01:16 -0600

    evolving correct solution - this solution catches dynamic quantities of items

--- lib/xkcd.rb
+++ lib/xkcd.rb
@@ -1,40 +1,55 @@
 class Xkcd
 
-  attr_reader :file, :prices
+  attr_reader :file, :menu, :prices
 
   def initialize(file = './data_file')
     @file = read_file(file)
-    @prices = []
+    @menu = {}
+    @prices
   end
 
   def find_combinations
-    find_prices
-    total_price = @prices.shift
-    total_items = self.prices.count
-    find_permutations(total_price, total_items)
+    set_up.reverse.each do |permutation|
+      total_bill = 0
+      permutation.each_with_index do |quantity, i|
+        next if total_bill > self.menu['total price']
+        total_bill += (self.prices[i] * quantity)
+      end
+      p permutation if total_bill == self.menu['total price']
+    end
   end
 
 private
+
+  def set_up
+    create_menu && create_prices
+    highest_possible_quantity = self.menu['total price'] / self.prices[0]
+    total_items = self.prices.count
+    find_repeated_permutations(highest_possible_quantity, total_items)
+  end
 
   def read_file(file)
     File.read(file)
   end
 
-  def find_prices
-    self.file.each_line { |line| @prices << line.gsub(/[^\d\.]/, '').to_f }
+  def create_menu
+    self.file.each_line do |line|
+      item = line.chomp.gsub(/[^A-Za-z\s]/, '')
+      price = line.gsub(/[^\d]/, '').to_i
+      @menu[item] = price unless item == ''
+      @menu['total price'] = price if item == ''
+    end
+    self.menu
   end
 
-  def find_permutations(total_price, total_items)
-    i = 1
-    while i <= total_items
-      self.prices.permutation.to_a(i).each do |one_combination|
-        if one_combination.reduce(:+) == total_price
-          return one_combination
-        end
-      end
-      i += 1
-    end
-    puts 'There were no matching combinations.'
+  def create_prices
+    @prices = @menu.values
+    @prices.shift
+    @prices = self.prices.sort
+  end
+
+  def find_repeated_permutations(highest_possible_quantity, total_items)
+    (0..highest_possible_quantity).to_a.repeated_permutation(total_items).to_a
   end
 
 end
